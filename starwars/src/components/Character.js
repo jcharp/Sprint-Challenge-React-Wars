2956961eb6a520f14663c764c401b48301b0cf8e
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-05-03 20:33:39 -0400

    not doing good

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import React, { useEffect, useState } from "react";
+import ReactDom from "react-dom";
+import axios from "axios";
+
+
+const Characters = () => {
+
+
+
+
+const [character, setCharacter] = useState([]);
+useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/")
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => console.log("error", error));
+}, []);
+
+}
+
+export default Characters;
